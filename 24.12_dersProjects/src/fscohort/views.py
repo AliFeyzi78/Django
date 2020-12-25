d0cfadd49dcdf53243593ceb14791fb78a9cdefc
AliFeyzi78 <zeron1g2k@gmail.com>
2020-12-25 16:55:32 +0200

    model oluşturduk

--- 24.12_dersProjects/src/fscohort/views.py
+++ 24.12_dersProjects/src/fscohort/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(request):
 	return HttpResponse("Hello, this is fscohort Homepage!!")
+
+def about_view(request):
+    return HttpResponse("Hello, this is fscohort Aboutpage!!")
