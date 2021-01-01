1a4237fde8bd3cb16b666d6fb1cb221a2a32aeb6
AliFeyzi78 <zeron1g2k@gmail.com>
2021-01-01 17:20:15 +0200

    finished

--- 26.12_dersProjects/src/fscohort/views.py
+++ 26.12_dersProjects/src/fscohort/views.py
@@ -39,7 +39,8 @@ def student_add(request):
 
 
 def student_detail(request, id):
-    student = Student.objects.get(id=id)
+    student = get_object_or_404(Student, id=id)
+    # student = Student.objects.get(id=id)
     context = {
         'student': student
     }
@@ -47,8 +48,8 @@ def student_detail(request, id):
 
 
 def student_delete(request, id):
-    # student = get_object_or_404(Student, id=id)
-    student = Student.objects.get(id=id)
+    student = get_object_or_404(Student, id=id)
+    # student = Student.objects.get(id=id)
     if request.method == "POST":
         student.delete()
         return redirect("list")
@@ -57,7 +58,8 @@ def student_delete(request, id):
 
 
 def student_update(request, id):
-    student = Student.objects.get(id=id)
+    student = get_object_or_404(Student, id=id)
+    # student = Student.objects.get(id=id)
     form = StudentForm(instance=student)
     if request.method == "POST":
         form = StudentForm(request.POST, instance=student)
@@ -65,7 +67,6 @@ def student_update(request, id):
             form.save()
             return redirect("list")
     context = {
-        'student': student,
         'form': form
     }
     return render(request, "fscohort/student_update.html", context)
